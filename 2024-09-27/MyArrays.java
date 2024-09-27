adf252b3553c176379eb37b9f378490e94c66305
abduls2008 <asiddique60@stuy.edu>
2024-09-27 10:01:53 -0400

    save

---
+++ 2024-09-27/MyArrays.java
@@ -0,0 +1,16 @@
+public class MyArrays{
+  public static String aryToString(int[] nums){
+    for (int i = 0; i< nums.length; i++){
+    String ans = "["
+    ans+= nums[i]+", ";
+
+  }
+  return ans + "]"
+}
+
+  public static int[] returnCopy(int[]ary)
+  
+
+
+
+}
