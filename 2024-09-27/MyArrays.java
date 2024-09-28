b235ca4796ff79d5cf72c7270b3db912df704b29
abduls2008 <asiddique60@stuy.edu>
2024-09-28 16:56:50 -0400

    home

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,29 +1,37 @@
 public class MyArrays{
   public static String aryToString(int[] nums){
+	String ans = "[";
     for (int i = 0; i< nums.length; i++){
-    String ans = "["
-    ans+= nums[i]+", ";
+	ans+= nums[i];
+	if (i< nums.length -1){
+		ans+=", ";
+	}
 
   }
-  return ans + "]"
+  return ans + "]";
 }
 
-  public static int[] returnCopy(int[]ary)
-    String[] builder;
-    for(i = 0; i<ary.length; i++){
-      builder+=ary[i]
+ // public static int[] returnCopy(int[]ary)
+  //  String[] builder;
+  //  for (i = 0; i<ary.length; i++){
+  //    builder+=ary[i];
 
 
-    }
+ //   }
 
 
 
-}
 
+  
   public static void main(String[] args){
-    System.out.println()
+	int[] one = {1};
+	int[] two = {1,5};
+    System.out.println(aryToString(one));
+	System.out.println(aryToString(two));
   }
+}
+  
 
 
 
-  public static int[] concatArray(int[]ary1,int[]ary2)
+ //public static int[] concatArray(int[]ary1,int[]ary2)
