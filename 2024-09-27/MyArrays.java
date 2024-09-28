d0d03473bb81bed881c903a8fa3e675ddc6d9350
abduls2008 <asiddique60@stuy.edu>
2024-09-28 17:24:55 -0400

    saved

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -11,27 +11,72 @@ public class MyArrays{
   return ans + "]";
 }
 
- // public static int[] returnCopy(int[]ary)
-  //  String[] builder;
-  //  for (i = 0; i<ary.length; i++){
-  //    builder+=ary[i];
-
-
- //   }
-
-
-
-
-  
-  public static void main(String[] args){
-	int[] one = {1};
-	int[] two = {1,5};
-    System.out.println(aryToString(one));
-	System.out.println(aryToString(two));
-  }
+	public static int[] returnCopy(int[] ary) {
+    // Create a new array of the same length
+		int[] copy = new int[ary.length];
+    
+    // Copy elements from ary to copy
+		for (int i = 0; i < ary.length; i++) {
+        copy[i] = ary[i];
+    }
+    
+		return copy;
 }
+
+
+  
+  public static int[] concatArray(int[] ary1, int[] ary2){
+	  int[] newarray = new int[ary1.length + ary2.length];
+	  for (int i=0; i<ary1.length; i++){
+		newarray[i] = ary1[i];
+	  }
+		
+	  for (int i=0; i<ary2.length; i++){
+		newarray[ary1.length + i] = ary2[i];
+	  }
+	  return newarray;
+  }
+
+
+  
+ //public static void main(String[] args){
+	//int[] one = {1};
+	//int[] two = {1,5};
+   // System.out.println(aryToString(one));
+	//System.out.println(aryToString(two));
+//  }
+
   
 
 
 
- //public static int[] concatArray(int[]ary1,int[]ary2)
+ public static void main(String[] args) {
+    
+    int[] originalArray = {1, 2, 3, 4, 5};
+    int[] copiedArray = returnCopy(originalArray);
+    
+    
+    System.out.println("Original Array:" + aryToString(originalArray));
+    System.out.println("Copied Array:" + aryToString(copiedArray));
+    
+    
+    
+
+   
+    originalArray[0] = 10;
+    System.out.println("Modified Original Array: " + aryToString(originalArray));
+    System.out.println("Copied Array after original modification: " + aryToString(copiedArray));
+    
+    
+    int[] array1 = {1, 2, 3};
+    int[] array2 = {4, 5, 6};
+    
+    int[] concatenatedArray = concatArray(array1, array2);
+    System.out.println("Concatenated Array: " + aryToString(concatenatedArray));
+
+    // check if arrays are unchanged
+    System.out.println("Array 1: " + aryToString(array1));
+    System.out.println("Array 2: " + aryToString(array2));
+}
+}
+
