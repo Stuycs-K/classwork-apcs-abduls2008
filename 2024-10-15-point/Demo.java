7df1f6e0b427c2ad1eacf5f123fcba36403a1592
abduls2008 <asiddique60@stuy.edu>
2024-10-15 20:48:18 -0400

    added equilateral triangle test

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -15,6 +15,9 @@ public class Demo{
     Point p4 = new Point(0,5);
     Point p5 = new Point(5,10);
     Point p6 = new Point(5,5);
+	Point triangle1 = new Point(0,0);
+	Point triangle2 = new Point(5,0);
+	Point triangle3 = new Point(2.5, 4.33);
     System.out.println( p3);
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
@@ -23,6 +26,9 @@ public class Demo{
     System.out.println( distance(p3,p4));
     System.out.println( Point.distance(p3,p4));
     System.out.println( p3.distanceTo(p4));
+	System.out.println( distance(triangle1,triangle2));
+    System.out.println( Point.distance(triangle2,triangle3));
+    System.out.println( triangle1.distanceTo(triangle3));
     ;
   }
 }
