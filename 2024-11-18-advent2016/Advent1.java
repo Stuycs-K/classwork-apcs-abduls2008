fadbca7b27be01a9ea1f4b2687c6cfa45f608134
abduls2008 <asiddique60@stuy.edu>
2024-11-18 10:14:34 -0500

    edits

--- 2024-11-18-advent2016/Advent1.java
+++ 2024-11-18-advent2016/Advent1.java
@@ -1,3 +1,29 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
+public class Advent1{
+  try {
+      Scanner fileScanner = new Scanner(new File(filename));
+      
+          lineScanner.close();
+      }
+      fileScanner.close();
+  } catch (FileNotFoundException e) {
+      System.out.println("File not found: " + filename);
+  }
+
+  public static blocksaway(){
+
+
+  }
+
+
+
+
+
+
+
+
+
+}
