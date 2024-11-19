1f743881534a39b01313a9a03f317d1cc34f236c
abduls2008 <asiddique60@stuy.edu>
2024-11-18 19:04:19 -0500

    fixed triangle methods

--- 2024-11-18-advent2016/Advent.java
+++ 2024-11-18-advent2016/Advent.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class TriangleTester {
+public class Advent {
 
 
  public static boolean isValidTriangle(int a, int b, int c) {
@@ -10,20 +10,31 @@ public class TriangleTester {
     }
 
 
+   
+
+    // Part A: Count triangles row by row
     public static int countTrianglesA(String filename) {
         int validCount = 0;
         try {
             Scanner fileScanner = new Scanner(new File(filename));
             while (fileScanner.hasNextLine()) {
                 String line = fileScanner.nextLine();
-                Scanner lineScanner = new Scanner(line);
-                int a = lineScanner.nextInt();
-                int b = lineScanner.nextInt();
-                int c = lineScanner.nextInt();
-                if (isValidTriangle(a, b, c)) {
-                    validCount++;
+                if (!line.isEmpty()) {
+                    Scanner lineScanner = new Scanner(line);
+                    if (lineScanner.hasNextInt()) {
+                        int a = lineScanner.nextInt();
+                        int b = lineScanner.nextInt();
+                        int c = lineScanner.nextInt();
+                        if (isValidTriangle(a, b, c)) {
+                            validCount++;
+                        }
+                    } else {
+                        System.out.println("Skipping line: " + line);
+                    }
+                    lineScanner.close();
+                } else {
+                    System.out.println("skipping emty line");
                 }
-                lineScanner.close();
             }
             fileScanner.close();
         } catch (FileNotFoundException e) {
@@ -32,40 +43,51 @@ public class TriangleTester {
         return validCount;
     }
 
-	public static int countTrianglesB(String filename) {
-        int validCount = 0;
-        try {
-            Scanner fileScanner = new Scanner(new File(filename));
-            while (fileScanner.hasNextLine()) {
-                int[][] side = new int[3][3];
-				for (int i=0; i<3; i++){
-					if (fileScanner.hasNextLine()){
-						Scanner lineScanner = new Scanner(fileScanner.nextLine());
-						side[i][0] = lineScanner.nextInt();
-						side[i][1] = lineScanner.nextInt();
-						side[i][2] = lineScanner.nextInt();
-						 lineScanner.close();
-					}
+public static int countTrianglesB(String filename) {
+    int validCount = 0;
+    try {
+        Scanner fileScanner = new Scanner(new File(filename));
+        while (fileScanner.hasNextLine()) {
+            int[][] sides = new int[3][3];
+            int filledRows = 0;
+
+            for (int i = 0; i < 3; i++) {
+                if (fileScanner.hasNextLine()) {
+                    String line = fileScanner.nextLine();
+                    Scanner lineScanner = new Scanner(line);
+
+                    
+                    if (lineScanner.hasNextInt()) {
+                        sides[i][0] = lineScanner.nextInt();
+                        if (lineScanner.hasNextInt()) sides[i][1] = lineScanner.nextInt();
+                        if (lineScanner.hasNextInt()) sides[i][2] = lineScanner.nextInt();
+                        filledRows++;
+                    } else {
+                        System.out.println("Skipping line " + line);
+                    }
+
+                    lineScanner.close();
                 }
-				for (int colm = 0; colm <3; colm++){
-					int a = side[0][colm], b = side[1][colm], c = side[2][colm];
+            }
+
+            
+            if (filledRows == 3) {
+                for (int col = 0; col < 3; col++) {
+                    int a = sides[0][col], b = sides[1][col], c = sides[2][col];
                     if (isValidTriangle(a, b, c)) {
                         validCount++;
-
-
+                    }
+                }
+            } else {
+                System.out.println("Skipping line");
             }
-
-			}
-		}
-            fileScanner.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found: " + filename);
         }
-        return validCount;
+        fileScanner.close();
+    } catch (FileNotFoundException e) {
+        System.out.println("File not found: " + filename);
     }
-
-
-
+    return validCount;
+}
 
  public static void main(String[] args) {
 
