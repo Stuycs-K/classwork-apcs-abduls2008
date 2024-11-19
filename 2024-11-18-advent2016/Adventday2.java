2839ac68921c935c43510af307cb44c57945a233
abduls2008 <asiddique60@stuy.edu>
2024-11-19 10:14:22 -0500

    attempted day 2

---
+++ 2024-11-18-advent2016/Adventday2.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Adventday2 {
+
+
+
+  public static String key(){
+    int[][] keypad = new int[3][3]
+    keypad[0][1]=
+    keypad[0][2]=
+    keypad[0[3]=
+    String code = "";
+    try {
+        Scanner fileScanner = new Scanner(new File(filename));
+        while (fileScanner.hasNextLine()) {
+            String line = fileScanner.nextLine();
+            if
+
+          }
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filename);
+        }
+
+
+
+  }
+
+
+
+
+
+
+}
