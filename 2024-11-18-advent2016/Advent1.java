8bbc6f7e5e5346d84c5d34cdb384eda8f63df8b9
abduls2008 <asiddique60@stuy.edu>
2024-11-18 19:40:16 -0500

    day 1 part 1

--- 2024-11-18-advent2016/Advent1.java
+++ 2024-11-18-advent2016/Advent1.java
@@ -2,28 +2,53 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Advent1{
-  try {
-      Scanner fileScanner = new Scanner(new File(filename));
-      
-          lineScanner.close();
-      }
-      fileScanner.close();
-  } catch (FileNotFoundException e) {
-      System.out.println("File not found: " + filename);
-  }
+public class Advent1 {
 
-  public static blocksaway(){
+    public static int calculateDistance(String filename) {
+        int x = 0, y = 0; 
+        int direction = 0; 
 
+        try {
+            Scanner fileScanner = new Scanner(new File(filename));
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                
+                
+                if (!line.isEmpty()) {
+                    
+                    String[] instructions = line.split(", ");
+                    for (String instruction : instructions) {
+                        char turn = instruction.charAt(0); 
+                        int steps = Integer.parseInt(instruction.substring(1)); 
 
-  }
+                        
+                        if (turn == 'R') {
+                            direction = (direction + 1) % 4;
+                        } else if (turn == 'L') {
+                            direction = (direction + 3) % 4;
+                        }
 
+                        
+                        switch (direction) {
+                            case 0: y += steps; break; // North
+                            case 1: x += steps; break; // East
+                            case 2: y -= steps; break; // South
+                            case 3: x -= steps; break; // West
+                        }
+                    }
+                }
+            }
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filename);
+        }
 
+       
+        return Math.abs(x) + Math.abs(y);
+    }
 
-
-
-
-
-
-
+    public static void main(String[] args) {
+        String filename = "input2.txt"; // Input file containing the directions
+        System.out.println("Distance to Easter Bunny HQ: " + calculateDistance(filename));
+    }
 }
