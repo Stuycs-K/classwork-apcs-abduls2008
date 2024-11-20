c6b81ec3c2c3788d9012501a714bd9ae92bfa8d4
abduls2008 <asiddique60@stuy.edu>
2024-11-19 20:45:34 -0500

    day2part1

--- 2024-11-18-advent2016/Adventday2.java
+++ 2024-11-18-advent2016/Adventday2.java
@@ -6,31 +6,55 @@ public class Adventday2 {
 
 
 
-  public static String key(){
-    int[][] keypad = new int[3][3]
-    keypad[0][1]=
-    keypad[0][2]=
-    keypad[0[3]=
+  public static String key(String filename){
+	  int row=1; // [1][1] is 5
+	  int colm=1;
+    int[][] keypad = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
     String code = "";
     try {
-        Scanner fileScanner = new Scanner(new File(filename));
-        while (fileScanner.hasNextLine()) {
-            String line = fileScanner.nextLine();
-            if
-
-          }
+            Scanner fileScanner = new Scanner(new File(filename));
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                for (int i = 0; i < line.length(); i++) {
+                    char dir = line.charAt(i);
+                    if (dir == 'U' && row > 0) {
+                        row = row - 1;
+                    } else if (dir == 'D' && row < 2) {
+                        row = row + 1;
+                    } else if (dir == 'L' && colm > 0) {
+                        colm = colm - 1;
+                    } else if (dir == 'R' && colm < 2) {
+                        colm = colm + 1;
+                    }
+                }
+              
+                code = code + keypad[row][colm];
+            }
             fileScanner.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + filename);
         }
 
-
-
-  }
-
-
-
-
-
-
+        return code;
+    }
+	public static void main(String[] args) {
+        
+        String filename = "inputday2.txt";
+        System.out.println("Bathroom code: " + key(filename));
+    }
 }
+	
+
+
+  
+
+
+
+
+
+
+
