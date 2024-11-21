8ff448ea99309656eaacabf0f7280808ac7827c0
abduls2008 <asiddique60@stuy.edu>
2024-11-20 20:29:10 -0500

    day4

--- 2024-11-18-advent2016/Adventday4.java
+++ 2024-11-18-advent2016/Adventday4.java
@@ -2,25 +2,85 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public boolean isValidRoom(String name, String checksum){
+public class Adventday4 {
 
+    public static int calculate(String fileName) {
+        int sum = 0;
 
-  int[] letcount = new int[26];
-  for (char c: name.toCharArray[]){
-    letcount[c-'a']++
-  }
-}
+        try {
+            File inputFile = new File(fileName);
+            Scanner scanner = new Scanner(inputFile);
 
-  for (int i=0; i< checksum.length(); i++){
-    char current = checksum.charAt(i);
-    for (int j = i + 1; j<checksum.length(); j++){
-      char next = checksum.charAt[i];
-      if (letcount[next - 'a'] > letcount[current - 'a'] || (letcount[next-'a'] == letcount[current - 'a'] && next < current))
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split("-");
+                String name = "";
+
+              
+                for (int i = 0; i < parts.length - 1; i++) {
+                    name += parts[i];
+                }
+
+                String lastPart = parts[parts.length - 1];
+                int openBracket = lastPart.indexOf('[');
+                int closeBracket = lastPart.indexOf(']');
+
+                if (openBracket != -1 && closeBracket != -1) {
+                    int sectorId = Integer.parseInt(lastPart.substring(0, openBracket));
+                    String checksum = lastPart.substring(openBracket + 1, closeBracket);
+
+                    
+                    if (isValidRoom(name, checksum)) {
+                        sum += sectorId;
+                    }
+                } else {
+                    
+                    System.out.println("error: " + line);
+                }
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + fileName);
+        }
+
+        return sum;
     }
 
-  }
-  return false;
-}
-}
-return true;
+    public static boolean isValidRoom(String name, String checksum) {
+        int[] letcount = new int[26];
+
+        
+        for (int i = 0; i < name.length(); i++) {
+            char c = name.charAt(i);
+            if (c >= 'a' && c <= 'z') { 
+                letcount[c - 'a']++;
+            }
+        }
+
+        
+        char[] newsort = new char[26];
+        int sortedIndex = 0;
+
+        for (int freq = 100; freq > 0; freq--) {
+            for (int i = 0; i < 26; i++) {
+                if (letcount[i] == freq) {
+                    newsort[sortedIndex++] = (char) (i + 'a');
+                }
+            }
+        }
+
+       
+        for (int i = 0; i < checksum.length(); i++) {
+            if (checksum.charAt(i) != newsort[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum: " + calculate("inputday4.txt"));
+    }
 }
