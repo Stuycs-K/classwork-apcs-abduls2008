0b7b8e361d54aeee786a6f95581b9b7d574dc940
abduls2008 <asiddique60@stuy.edu>
2024-11-23 15:58:38 -0500

    part 2

--- 2024-11-18-advent2016/Advent1.java
+++ 2024-11-18-advent2016/Advent1.java
@@ -46,9 +46,73 @@ public class Advent1 {
        
         return Math.abs(x) + Math.abs(y);
     }
+	  public static int part2(String filename) {
+        int x = 0, y = 0;
+        int direction = 0;
+        int[][] visitedLocations = new int[10000][2];
+        int visitedCount = 1;
+        visitedLocations[0][0] = x;
+        visitedLocations[0][1] = y;
 
-    public static void main(String[] args) {
-        String filename = "input2.txt"; // Input file containing the directions
+        try {
+            Scanner fileScanner = new Scanner(new File(filename));
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                if (!line.isEmpty()) {
+                    String[] instructions = line.split(", ");
+                    for (int i = 0; i < instructions.length; i++) {
+                        String instruction = instructions[i];
+                        char turn = instruction.charAt(0);
+                        int steps = Integer.parseInt(instruction.substring(1));
+
+                        if (turn == 'R') {
+                            direction = (direction + 1) % 4;
+                        } else if (turn == 'L') {
+                            direction = (direction + 3) % 4;
+                        }
+
+                        for (int j = 0; j < steps; j++) {
+                            if (direction == 0) {
+                                y++;
+                            } else if (direction == 1) {
+                                x++;
+                            } else if (direction == 2) {
+                                y--;
+                            } else if (direction == 3) {
+                                x--;
+                            }
+
+                            boolean visited = false;
+                            for (int k = 0; k < visitedCount; k++) {
+                                if (visitedLocations[k][0] == x && visitedLocations[k][1] == y) {
+                                    visited = true;
+                                    break;
+                                }
+                            }
+
+                            if (visited) {
+                                return Math.abs(x) + Math.abs(y);
+                            }
+
+                            visitedLocations[visitedCount][0] = x;
+                            visitedLocations[visitedCount][1] = y;
+                            visitedCount++;
+                        }
+                    }
+                }
+            }
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filename);
+        }
+
+        return -1;
+    }
+
+   public static void main(String[] args) {
+        String filename = "input2.txt"; 
         System.out.println("Distance to Easter Bunny HQ: " + calculateDistance(filename));
+        System.out.println("part2: " + part2(filename));
+		
     }
 }
--- 2024-11-18-advent2016/Adventday2.java
+++ 2024-11-18-advent2016/Adventday2.java
@@ -41,12 +41,64 @@ public class Adventday2 {
 
         return code;
     }
-	public static void main(String[] args) {
-        
+	public static String keyPart2(String filename) {
+        char[][] keypad = {
+                {'0', '0', '1', '0', '0'},
+                {'0', '2', '3', '4', '0'},
+                {'5', '6', '7', '8', '9'},
+                {'0', 'A', 'B', 'C', '0'},
+                {'0', '0', 'D', '0', '0'}
+        };
+
+        int row = 2; 
+        int colm = 0;
+        String code = "";
+
+        try {
+            Scanner fileScanner = new Scanner(new File(filename));
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                for (char dir : line.toCharArray()) {
+                    int newRow = row;
+                    int newColm = colm;
+
+                    if (dir == 'U') {
+                        newRow--;
+                    } else if (dir == 'D') {
+                        newRow++;
+                    } else if (dir == 'L') {
+                        newColm--;
+                    } else if (dir == 'R') {
+                        newColm++;
+                    }
+
+                    
+                    if (newRow >= 0 && newRow < 5 && newColm >= 0 && newColm < 5 && keypad[newRow][newColm] != '0') {
+                        row = newRow;
+                        colm = newColm;
+                    }
+                }
+                code = code + keypad[row][colm]; 
+            }
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filename);
+        }
+
+        return code;
+    }
+
+    public static void main(String[] args) {
         String filename = "inputday2.txt";
-        System.out.println("Bathroom code: " + key(filename));
+
+       
+        System.out.println("Bathroom Code (Part 1): " + key(filename));
+
+        
+        System.out.println("Bathroom Code (Part 2): " + keyPart2(filename));
     }
 }
+
 	
 
 
