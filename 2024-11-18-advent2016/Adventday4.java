37ecd11b4eb8bbc9b676f98f4ff58ef244af5aaa
abduls2008 <asiddique60@stuy.edu>
2024-11-24 22:25:09 -0500

    added part2

--- 2024-11-18-advent2016/Adventday4.java
+++ 2024-11-18-advent2016/Adventday4.java
@@ -79,8 +79,63 @@ public class Adventday4 {
 
         return true;
     }
+	
+    public static int part2(String fileName) {
+        try {
+            File inputFile = new File(fileName);
+            Scanner scanner = new Scanner(inputFile);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split("-");
+                String name = "";
+
+                for (int i = 0; i < parts.length - 1; i++) {
+                    name += parts[i];
+                }
+
+                String lastPart = parts[parts.length - 1];
+                int openBracket = lastPart.indexOf('[');
+                int closeBracket = lastPart.indexOf(']');
+
+                if (openBracket != -1 && closeBracket != -1) {
+                    int sectorId = Integer.parseInt(lastPart.substring(0, openBracket));
+                    String checksum = lastPart.substring(openBracket + 1, closeBracket);
+
+                    if (isValidRoom(name, checksum)) {
+                        String decryptedName = decryptName(line.substring(0, line.lastIndexOf('-')), sectorId);
+
+                        if (decryptedName.equals("northpole object storage")) {
+                            return sectorId;
+                        }
+                    }
+                }
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + fileName);
+        }
+
+        return -1;
+    }
+
+    public static String decryptName(String encryptedName, int sectorId) {
+        String result = "";
+        for (int i = 0; i < encryptedName.length(); i++) {
+            char c = encryptedName.charAt(i);
+            if (c == '-') {
+                result += " ";
+            } else if (c >= 'a' && c <= 'z') {
+                int newChar = ((c - 'a' + sectorId) % 26) + 'a';
+                result += (char) newChar;
+            }
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Sum: " + calculate("inputday4.txt"));
+        System.out.println("Sum of sector IDs: " + calculate("inputday4.txt"));
+        System.out.println("North Pole Room Sector ID: " + part2("inputday4.txt"));
     }
 }
