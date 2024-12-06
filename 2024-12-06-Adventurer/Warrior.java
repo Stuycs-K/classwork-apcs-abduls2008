00546cfdf07ba57463fda37498c5ed8234c362e7
abduls2008 <asiddique60@stuy.edu>
2024-12-06 10:05:01 -0500

    added special

--- 2024-12-06-Adventurer/Warrior.java
+++ 2024-12-06-Adventurer/Warrior.java
@@ -1,7 +1,23 @@
 import java.util.*;
+private int rage;
 public class Warrior extends Adventurer{
 
   public Warrior(String name){
     this(name, 20);
+    this.rage = 2;
+
   }
+  public String getSpecialName();{
+
+  }
+
+  public int getSpecial();{
+    return rage;
+
+  }
+  public void setSpecial(int n);{
+  rage=n;
+}
+
+
 }
