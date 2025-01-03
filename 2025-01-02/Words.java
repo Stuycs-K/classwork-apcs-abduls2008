e56c9c5d36079703684723a8c2389aa3378a389c
abduls2008 <asiddique60@stuy.edu>
2025-01-03 10:12:37 -0500

    attempted to add function

---
+++ 2025-01-02/Words.java
@@ -0,0 +1,27 @@
+public class Words{
+
+
+
+
+
+
+
+
+public static void makeWords(int remainingLetters, String result, String alphabet){
+  if(remainingLetters == 0){
+    System.out.println("");
+
+  }
+  result =;
+  System.out.println(makeWords(remainingLetters-1, ,alphabet));
+
+  }
+}
+
+
+
+
+ public static void main(String[] args){
+   System.out.println(makeWords(2, "", abc));
+ }
+}
